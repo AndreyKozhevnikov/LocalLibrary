36dee4da13cb57bfea64494eff141b11013683f3
AndreyK <andrey.kozhevnikov@devexpress.com>
2017-07-17 08:05:13 +0300

    some changes

---
+++ ToStart.bat
@@ -0,0 +1,1 @@
+SET DEBUG=locallibrary:* & npm run devstart
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'ExpressN' });
+});
+
+router.get('/testRoute', function(req, res, next) {
+  res.render('index3', { title: 'ExpressN' });
 });
 
 module.exports = router;
