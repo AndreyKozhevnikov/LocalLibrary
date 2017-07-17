07db7eb8f07c2b49d177b7fd667c80ea2f6cbdb8
Andrey <freazeek@mail.ru>
2017-07-17 21:52:50 +0300

    Revert "install mongoose"
    This reverts commit f0759e0bb51b3fc6b4834f03ef984589a923048b.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'ExpressN45' });
+  res.render('index', { title: 'ExpressN' });
 });
 
 router.get('/testRoute', function(req, res, next) {
