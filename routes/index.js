40917f0aa2b994ef4d29b3e314e6240407aae065
AndreyK <andrey.kozhevnikov@devexpress.com>
2017-07-18 08:49:14 +0300

    touch routes

--- routes/index.js
+++ routes/index.js
@@ -9,5 +9,12 @@ router.get('/', function(req, res, next) {
 router.get('/testRoute', function(req, res, next) {
   res.render('index3', { title: 'ExpressN' });
 });
-
+router.get('/ab+cd', function(req, res, next) {
+  res.send('show route paths patterns (abcd, abbcd)')
+});
+router.get('/users/:userId/books/:bookId', function (req, res) {
+  // Access userId via: req.params.userId
+  // Access bookId via: req.params.bookId
+  res.send('show router parameters'+ req.params.bookId+req.params.userId);
+})
 module.exports = router;
