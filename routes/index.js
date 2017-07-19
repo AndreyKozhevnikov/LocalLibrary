6f1bcce7874a426b49a41ef9cbea8ec6bc6b2199
AndreyK <andrey.kozhevnikov@devexpress.com>
2017-07-19 09:07:23 +0300

    start pug

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-// router.get('/', function(req, res, next) {
-  // res.render('index', { title: 'ExpressN' });
-// });
+router.get('/oldindex', function(req, res, next) {
+   res.render('index', { title: 'expressn' });
+});
 
 /* GET home page. */
 router.get('/', function(req, res) {
