4aaffdfb5395df2fff8773d8d8e042a8c67ecd2f
AndreyK <andrey.kozhevnikov@devexpress.com>
2017-07-19 07:59:01 +0300

    routes and controllers

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,13 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'ExpressN' });
+// router.get('/', function(req, res, next) {
+  // res.render('index', { title: 'ExpressN' });
+// });
+
+/* GET home page. */
+router.get('/', function(req, res) {
+  res.redirect('/catalog');
 });
 
 router.get('/testRoute', function(req, res, next) {
